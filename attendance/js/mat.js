a9e2692412cc3151b2db644e6743e2192260c575
ashishlijhara <ashishlijhara@gmail.com>
2019-10-29 22:54:34 +0530

    added navbar

--- attendance/js/mat.js
+++ attendance/js/mat.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {direction:'top', hoverEnabled:false});
+
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge:'left', draggable:true});
+
   });
