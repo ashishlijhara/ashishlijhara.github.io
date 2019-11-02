d97f5be60f45d7571292682db748fd05111285c1
ashishlijhara <ashishlijhara@gmail.com>
2019-11-02 18:29:02 +0530

    fix for cam switch on mobile

--- attendance/js/codereader.js
+++ attendance/js/codereader.js
@@ -6,32 +6,27 @@ window.addEventListener('load', function () {
     .then((videoInputDevices) => {
       const sourceSelect = document.getElementById('switchCam')
       selectedDeviceId = videoInputDevices[0].deviceId
-      if (videoInputDevices.length >= 1 &&  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
-      {
-        for(i=0;i<videoInputDevices.length;i++){
-          if(videoInputDevices[i].label.includes('back')){
-            selectedDeviceId = i;
-          }
-        }
-      }
-        //sourceSelect.style.display = 'block';
+      startCam()
+      if (videoInputDevices.length >= 1) {
+        let selectedId = 0;
+        sourceSelect.style.display = 'block';
         /*videoInputDevices.forEach((element) => {
           const sourceOption = document.createElement('option')
           sourceOption.text = element.label
           sourceOption.value = element.deviceId
           sourceSelect.appendChild(sourceOption)
-        })
+        })*/
         sourceSelect.onclick = () => {
           codeReader.reset()
           selectedId+=1
           if(selectedId>=videoInputDevices.length)
             selectedId = 0
-          selectedDeviceId = selectedId
+          selectedDeviceId = videoInputDevices[selectedId].deviceId
           startCam()
-        };*/
-        startCam()
+        };
         //const sourceSelectPanel = document.getElementById('sourceSelectPanel')
         //sourceSelectPanel.style.display = 'block'
+      }
       function startCam(){
         codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
           if (result) {
