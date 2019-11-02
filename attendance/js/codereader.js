0934f59765f59fbf168535532a3fc73b0908dca9
ashishlijhara <ashishlijhara@gmail.com>
2019-11-02 15:49:56 +0530

    cam switching logic

--- attendance/js/codereader.js
+++ attendance/js/codereader.js
@@ -4,41 +4,65 @@ window.addEventListener('load', function () {
   console.log('ZXing code reader initialized')
   codeReader.getVideoInputDevices()
     .then((videoInputDevices) => {
-      const sourceSelect = document.getElementById('sourceSelect')
-      selectedDeviceId = videoInputDevices[0].deviceId
-      if (videoInputDevices.length >= 1) {
-        videoInputDevices.forEach((element) => {
+      const sourceSelect = document.getElementById('switchCam')
+      const selectedDeviceId = videoInputDevices[0].deviceId
+      if (videoInputDevices.length > 1) {
+        let selectedId = 0;
+        sourceSelect.style.display = 'block';
+        /*videoInputDevices.forEach((element) => {
           const sourceOption = document.createElement('option')
           sourceOption.text = element.label
           sourceOption.value = element.deviceId
           sourceSelect.appendChild(sourceOption)
-        })
-        sourceSelect.onchange = () => {
-          selectedDeviceId = sourceSelect.value;
+        })*/
+        sourceSelect.onclick = () => {
+          codeReader.reset()
+          selectedId+=1
+          if(selectedId>videoInputDevices.length)
+            selectedId = 0
+          selectedDeviceId = selectedId
+          startCam()
         };
-        const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-        sourceSelectPanel.style.display = 'block'
+        //const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+        //sourceSelectPanel.style.display = 'block'
       }
-      document.getElementById('startButton').addEventListener('click', () => {
+      function startCam(){
         codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
           if (result) {
             console.log(result)
-            document.getElementById('result').textContent = result.text
+            //document.getElementById('result').textContent = result.text
           }
           if (err && !(err instanceof ZXing.NotFoundException)) {
             console.error(err)
-            document.getElementById('result').textContent = err
+            //document.getElementById('result').textContent = err
           }
         })
         console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+      }
+      function startCam(){
+        codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+          if (result) {
+            console.log(result)
+            //document.getElementById('result').textContent = result.text
+          }
+          if (err && !(err instanceof ZXing.NotFoundException)) {
+            console.error(err)
+            //document.getElementById('result').textContent = err
+          }
+        })
+        console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+      }
+      document.getElementById('startButton').addEventListener('click', () => {
+        startCam()
       })
       document.getElementById('resetButton').addEventListener('click', () => {
         codeReader.reset()
-        document.getElementById('result').textContent = '';
+        //document.getElementById('result').textContent = '';
         console.log('Reset.')
       })
     })
     .catch((err) => {
       console.error(err)
     })
+
 })
